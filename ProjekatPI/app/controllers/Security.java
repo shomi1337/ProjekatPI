e99d67e3a96cd9c6f54ed1be1925f545ebf67469
shomi1337 <novakovic.va@gmail.com>
2017-02-23 22:14:29 +0100

    Pristup svim clanovima tima

--- ProjekatPI/app/controllers/Security.java
+++ ProjekatPI/app/controllers/Security.java
@@ -7,11 +7,11 @@ public class Security extends Secure.Security {
 	static boolean authenticate(String username, String password) {
 		if (username.equals("milos") && password.equals("admin"))
 			return true;
-		else if (username.equals("milos") && password.equals("admin"))
+		else if (username.equals("miladin") && password.equals("admin"))
 			return true;
-		else if (username.equals("milos") && password.equals("admin"))
+		else if (username.equals("marko") && password.equals("admin"))
 			return true;
-		else if (username.equals("milos") && password.equals("admin"))
+		else if (username.equals("nemanja") && password.equals("admin"))
 			return true;
 		return false;
     }
